b4287407686bf22609398ae9e43ea993be006ad4
Ricardo <ricardorzan@gmail.com>
2021-10-27 22:24:31 -0500

    Modulo Condicionales ejercicio 1

---
+++ Condicionales/Ejercicio1/ejercicio1.js
@@ -0,0 +1,10 @@
+let dato1, dato2, num1, num2, resultado;
+dato1 = window.prompt("Primer número?", "0");
+num1 = parseInt(dato1);
+dato2 = window.prompt("Segundo número?", "0");
+num2 = parseInt(dato2);
+if (num1 > num2) {
+    document.write(` <h3>El mayor es el numero ${resultado} </h3> `);
+} else {
+    document.write(` <h3>El mayor es el numero ${resultado} </h3> `);
+}
